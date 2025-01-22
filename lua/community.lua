6e1d98d9f3d8cee2e596f9a53f19f6ddac0e0b5e
Jomar Milan <jomarm@jomarm.com>
2025-01-21 21:03:54 -0800

    Add typst pack

--- lua/community.lua
+++ lua/community.lua
@@ -8,6 +8,7 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.typst" },
   { import = "astrocommunity.media.vim-wakatime" },
   { import = "astrocommunity.terminal-integration.toggleterm-manager-nvim" },
   -- import/override with your plugins folder
